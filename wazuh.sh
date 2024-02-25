dd3b348ee3756cbb5b2811477a63178417a13633
np5yzb3 <156908671+np5yzb3@users.noreply.github.com>
2024-02-25 01:29:56 +0100

    wazuh.sh

--- wazuh.sh
+++ wazuh.sh
@@ -1,14 +1,14 @@
-#ESTE SCRIPT SE TIENE QUE EJECUTAR CON SUDO SU#
+#EJECUTA ESTE SCRIPT CON SUDO SU / RUN THIS WITH SUDO SU#
 #----------Wazuh indexer-----------#
 apt update
 curl -sO https://packages.wazuh.com/4.7/wazuh-certs-tool.sh
 curl -sO https://packages.wazuh.com/4.7/config.yml
 sed -i 's/node-1/wazuh-index/g' config.yml
-sed -i 's/<indexer-node-ip>/192.168.1.221/g' config.yml
+sed -i 's/<indexer-node-ip>/192.168.1.221/g' config.yml #Change the ip#
 sed -i 's/wazuh-1/wazuh-server/g' config.yml
-sed -i 's/<wazuh-manager-ip>/192.168.1.221/g' config.yml
+sed -i 's/<wazuh-manager-ip>/192.168.1.221/g' config.yml #Change the ip#
 sed -i 's/- name: dashboard/- name: wazuh-dash/g' config.yml
-sed -i 's/<dashboard-node-ip>/192.168.1.221/g' config.yml
+sed -i 's/<dashboard-node-ip>/192.168.1.221/g' config.yml #Change the ip#
 bash ./wazuh-certs-tool.sh -A
 tar -cvf ./wazuh-certificates.tar -C ./wazuh-certificates/ .
 cp wazuh-certificates.tar wazuh-certificates-cp.tar
@@ -19,7 +19,7 @@ curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring 
 echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 apt-get update
 apt-get -y install wazuh-indexer
-sed -i 's/0.0.0.0/192.168.1.221/g' /etc/wazuh-indexer/opensearch.yml
+sed -i 's/0.0.0.0/192.168.1.221/g' /etc/wazuh-indexer/opensearch.yml #Change the ip#
 sed -i 's/node-1/wazuh-index/g' /etc/wazuh-indexer/opensearch.yml
 NODE_NAME=wazuh-index
 mkdir /etc/wazuh-indexer/certs
@@ -40,7 +40,7 @@ systemctl enable wazuh-manager
 systemctl start wazuh-manager
 apt-get -y install filebeat
 curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.7/tpl/wazuh/filebeat/filebeat.yml
-sed -i 's/127.0.0.1/192.168.1.221/g' /etc/filebeat/filebeat.yml
+sed -i 's/127.0.0.1/192.168.1.221/g' /etc/filebeat/filebeat.yml #Change the ip#
 filebeat keystore create
 echo admin | filebeat keystore add username --stdin --force
 echo admin | filebeat keystore add password --stdin --force
@@ -61,8 +61,8 @@ systemctl start filebeat
 #----------Wazuh dashboard-----------#
 apt-get install debhelper tar curl libcap2-bin #debhelper version 9 or later
 apt-get -y install wazuh-dashboard
-sed -i 's/0.0.0.0/192.168.1.221/g' /etc/wazuh-dashboard/opensearch_dashboards.yml
-sed -i 's/localhost/192.168.1.221/g' /etc/wazuh-dashboard/opensearch_dashboards.yml
+sed -i 's/0.0.0.0/192.168.1.221/g' /etc/wazuh-dashboard/opensearch_dashboards.yml #Change the ip#
+sed -i 's/localhost/192.168.1.221/g' /etc/wazuh-dashboard/opensearch_dashboards.yml #Change the ip#
 NODE_NAME=wazuh-dash
 mkdir /etc/wazuh-dashboard/certs
 tar -xf ./wazuh-certificates.tar -C /etc/wazuh-dashboard/certs/ ./$NODE_NAME.pem ./$NODE_NAME-key.pem ./root-ca.pem
@@ -75,6 +75,4 @@ systemctl daemon-reload
 systemctl enable wazuh-dashboard
 systemctl start wazuh-dashboard
 
-/usr/share/wazuh-indexer/bin/indexer-security-init.sh
-
-#En el navegador: https://192.168.1.221#
+#En el navegador / In the browser: https://192.168.1.221#
